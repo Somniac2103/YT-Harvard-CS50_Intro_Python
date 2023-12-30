a850efa78621336fa684473fb132a400f1e1058b
Somniac2103 <salmon.barry1@gmail.com>
2023-12-30 13:15:24 +0700

    Continue with Python Training You Tube Course CS50 Intro to Programming with Python by Harvard
    Completed today:
    Working with strings, Int and Floats
    Methods
    Type casting
    Functions

---
+++ calculator.py
@@ -0,0 +1,16 @@
+#x = int(input("X Value: "))
+#y = int(input("Y Value: "))
+x = float(input("X Value: "))
+y = float(input("Y Value: "))
+
+#print(x + y)
+
+#z= round(x+y)
+#z= round(x / y, 2)
+z= x / y
+
+
+#print(f"{z:,}")
+print(f"{z:.2f}")
+#print(z)
+
--- hello.py
+++ hello.py
@@ -1,4 +1,25 @@
-name = input("What's your name? ")
+#STRING
+""" 
+name = input("What's your name? ").strip().title()
 print ("hello, " + name)
 print ("hello,", name)
 # Note to myself
+name = name.strip()
+name = name.capitalize()
+name = name.strip().title()
+first, last = name.split(" ")
+
+print(f"Hello, {first}")
+
+"""
+def hello(to = "world"):
+  print("Hello, " + to)
+
+
+name=input("What's ypu name? ")
+hello()
+hello(name)
+
+
+
+
